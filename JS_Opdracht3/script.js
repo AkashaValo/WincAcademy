0c56ddc50502f97fdd5678283bc53c45efc3e249
Leandra <87638656+AkashaValo@users.noreply.github.com>
2021-09-11 20:26:02 +0200

    JavaScript Opdracht 3
    JavaScript Opdracht 3

--- JS_Opdracht3/script.js
+++ JS_Opdracht3/script.js
@@ -1,1 +1,30 @@
+const age = 20;
+const isFemale = true;
+const driveStatus = "bob";
 
+if (age >= 18)
+{
+    console.log("Welcome. Please enter and have a good time");
+}
+else
+{
+    console.log("You are too young. Please stay outside");
+}
+
+if (isFemale)
+{
+    console.log("Person is female");
+}
+else
+{
+    console.log("Person is not female");
+}
+
+if (driveStatus == "bob")
+{
+    console.log("Person is sober and may drive");
+}
+else
+{
+    console.log("Person is not sober and may not drive");
+}
