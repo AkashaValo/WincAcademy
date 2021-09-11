d5753c9e13e9669bd1b971e4c55a024e6a1cfb61
Leandra <87638656+AkashaValo@users.noreply.github.com>
2021-09-11 19:45:11 +0200

    JavaScript Opdracht 1
    JavaScript Opdracht 1

--- JS_Opdracht1/script.js
+++ JS_Opdracht1/script.js
@@ -1,18 +1,24 @@
+console.log("Hello Winc Academy");
+
 let name = "Leandra";
+console.log(name);
+
 let sumNumbers = 4 + 4 ;
 let sumStrings = "string1" + "string2";
+console.log(sumNumbers);
+console.log(sumStrings);
+
 let addition = 1 + 1;
 let substraction = 5 - 3;
 let multiplication = 5 * 3;
 let division = 8 / 4;
 let modulus = 8 % 3;
-let age = "achtendertig";
-console.log(name);
-console.log(sumNumbers);
-console.log(sumStrings);
+
 console.log(addition);
 console.log(substraction);
 console.log(multiplication);
 console.log(division);
 console.log(modulus);
+
+let age = "achtendertig";
 console.log(typeof(age));
