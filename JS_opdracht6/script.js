e40dc38efeaab2c9a156f48c0321280bf6b9c145
Leandra <87638656+AkashaValo@users.noreply.github.com>
2021-09-15 20:23:05 +0200

    Javascript Opdracht 6 - Functions
    Javascript Opdracht 6 - Functions

--- JS_opdracht6/script.js
+++ JS_opdracht6/script.js
@@ -1,1 +1,25 @@
+const makingPizza = function() {
+    console.log('Rolling the dough');
+    console.log('Add tomato sauce');
+    console.log('Add onion bits');    
+    console.log('Add pepperoni');
+    console.log('Add cheese');    
+};
 
+const makingSushi = function() {
+    console.log('Fill nori with rice');
+    console.log('Add filling');
+    console.log('Roll sushi');    
+    console.log('Cut roll in little pieces');   
+};
+
+// answer question 7
+// No, we've only created a function, but not called it yet
+
+//3 order of sushi
+makingSushi();
+makingSushi();
+makingSushi();
+// 2 orders of pizza
+makingPizza();
+makingPizza();
