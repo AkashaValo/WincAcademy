9b3873832cee94926c85f0cf59ad5bf5928f57a8
Leandra <87638656+AkashaValo@users.noreply.github.com>
2021-09-22 20:11:51 +0200

    While and For Loops

--- JS_Opdracht11/even-odd-reporter.js
+++ JS_Opdracht11/even-odd-reporter.js
@@ -1,1 +1,8 @@
-
+for (i = 1; i <= 20; i++) {
+    if (i%2 == 0) {
+        console.log('Number', i, 'is even');
+    }
+    else {
+        console.log('Number', i, 'is odd');
+    }
+}
---
+++ JS_Opdracht11/multiplication-tables.js
@@ -0,0 +1,10 @@
+for (i = 1; i <= 10; i++) {
+   console.log(i, 'x 9 =', i * 9);
+}
+
+//bonus
+for (i = 1; i <= 10; i++) {
+    for (j = 1; j <= 10; j++) {
+        console.log(i, 'x', j,'=', i * j);
+    }
+}
