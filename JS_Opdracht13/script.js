c7096f7de8d4b88197b8bb779918b617583e74db
Leandra <87638656+AkashaValo@users.noreply.github.com>
2021-09-28 21:31:27 +0200

    Guess the number + Bonus

--- JS_Opdracht13/script.js
+++ JS_Opdracht13/script.js
@@ -7,25 +7,56 @@ const checkNumber = function (numberToGuess,promptNumber) {
   }
 }
 
-const guessNumber = function (numberToGuess) {
+const guessNumber = function (numberToGuess,min,max) {
   let guessed = false;
   let promptNumber = 0;
   let noOfGuesses = 5;
-  promptNumber = prompt('Give a number between 0 and 25?');
+  promptNumber = prompt('Give a number between '+ min +' and '+ max +'?');
   for (let i = 1; i < noOfGuesses; i++) {
-      promptNumber = prompt('Too bad. Only '+ (noOfGuesses - i) +' guess(es) left');   
       guessed = checkNumber(numberToGuess,promptNumber);
-      if (guessed == true) {break};  
+      if (guessed == true) {
+        break;
+      }
+      else {  
+        promptNumber = prompt('Too bad. Only '+ (noOfGuesses - i) +' guess(es) left');   
+      };
     }
   return guessed;
 }
 
+const getRange = function () {
+  let min;
+  let max;
+  do {
+    min = prompt('Please give the lowest number of the range?',1);
+    max = prompt('Please give the highest number of the range?',25);
+    console.log('mi value',min);
+    if (min == null) {min = 1};
+    if (max == null) {max = 25};
+    if (min >= max) {
+      alert('Maximum number must be larger than minimum number. Please re-enter.');
+    }
+  }
+  while (min >= max);
+  min = parseInt(min);
+  max = parseInt(max);
+  return [min,max];
+}
+
+const randomIntFromInterval = (minNumber, maxNumber) => Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+
+
 const startNewGame = function() {
-  const numberToGuess = Math.floor(Math.random() * 26);
-  const name = prompt('Hello, What is your name?');
+  let name = prompt('Hello, What is your name?');
+  if (name == null) {alert('Too bad you dont wanna play, maybe next time'); return;};
+  if (name == '') {name = 'Stranger'};
   alert('Hello ' + name + '.');
-  alert(numberToGuess);
-  let guessed = guessNumber(numberToGuess);
+  const minMax = getRange();
+  const min = minMax[0];
+  const max = minMax[1];
+  const numberToGuess = randomIntFromInterval(min,max);
+  console.log(numberToGuess);
+  let guessed = guessNumber(numberToGuess,min,max);
   if (guessed == true) {
     alert('Well done. You have guessed the correct number');
   }
